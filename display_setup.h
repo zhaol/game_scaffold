fd064ef1458e48563a95d70185986c2bc8c55955
Zhao Li <zhaol@hawaii.edu>
2013-09-29 17:18:28 -0400

    rearrange display_setup.h

--- display_setup.h
+++ display_setup.h
@@ -1,10 +1,10 @@
-#include <termios.h>    //termios, TCSANOW, ECHO, ICANON
-#include <unistd.h>     //STDIN_FILENO
-
 /*
-EE160 Students, you can ignore the contents of this file.  If you are curious, you can read this SO post for more information:
+EE160 Students, you can ignore the contents of this file.  If you are curious, you can read this StackOverflow post for more information:
 //http://stackoverflow.com/questions/1798511/how-to-avoid-press-enter-with-any-getchar
 */
+
+#include <termios.h>    //termios, TCSANOW, ECHO, ICANON
+#include <unistd.h>     //STDIN_FILENO
 
 #define SETUP_DISPLAY static struct termios oldt, newt;         \
                       tcgetattr( STDIN_FILENO, &oldt);          \
